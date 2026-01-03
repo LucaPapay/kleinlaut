caf29bfeefc4201a1152c6a8ae364685aee48471
Luca Papay <luca.papay99@gmail.com>
2026-01-03 14:31:42 +0100

    changes raumplan size

--- src/pages/RaumplanPage.vue
+++ src/pages/RaumplanPage.vue
@@ -1,8 +1,8 @@
 <template>
-  <section class="h-[100dvh] overflow-hidden bg-black">
+  <section class="h-[100vh] overflow-hidden bg-black">
     <TopBar />
-    <div class="flex h-[calc(100dvh-48px)] flex-col items-center justify-center px-4">
-      <div class="w-full max-w-[300px] md:max-w-md">
+    <div class="flex h-full flex-col items-center pt-12">
+      <div class="w-full max-w-[320px] md:max-w-md">
         <img class="h-auto w-full" :src="raumplanImage" alt="Raumplan" />
       </div>
     </div>
